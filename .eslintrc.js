d008070464a70b90e5d34852e878c676d580c068
CaoLei <1136208091@qq.com>
2020-02-17 09:10:38 +0800

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ['plugin:vue/essential', '@vue/standard'],
+    rules: {
+        // 强制使用一致的缩进
+        indent: 0,
+        // 单引号
+        quotes: ['error', 'single'],
+        // 要求或禁止使用分号而不是 ASI
+        semi: ['error', 'never'],
+        // 禁止在字符串和注释之外不规则的空白
+        'no-irregular-whitespace': 0,
+        // 禁用行尾空格
+        'no-trailing-spaces': 0,
+        // 函数名称或function关键字与开始参数之间允许有空格
+        'space-before-function-paren': 0,
+        // 不允许在大括号之间留出空格
+        'object-curly-spacing': ['error', 'never'],
+        // 允许注释//后空白
+        'spaced-comment': 0,
+        // 允许省略花括号
+        curly: 0,
+        // 允许只用一个var定义变量
+        'one-var': 0,
+        'no-sequences': 0,
+        'handle-callback-err': 0
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
+}
